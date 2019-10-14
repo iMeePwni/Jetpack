134aaf7b441af745459b52d7ecbce45b07ff9137
imeepwni <imeepwni@outlook.com>
2019-10-14 14:29:12 +0800

    init encrypted file.

--- app/src/main/java/com/imeepwni/jetpack/app/BaseActivity.kt
+++ app/src/main/java/com/imeepwni/jetpack/app/BaseActivity.kt
@@ -1,5 +1,6 @@
 package com.imeepwni.jetpack.app
 
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -8,4 +9,8 @@ import androidx.appcompat.app.AppCompatActivity
  */
 abstract class BaseActivity : AppCompatActivity() {
 
+}
+
+internal fun BaseActivity.toast(text: String) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
 }
--- app/src/main/java/com/imeepwni/jetpack/function/encrypted_file/EncryptedFileActivity.kt
+++ app/src/main/java/com/imeepwni/jetpack/function/encrypted_file/EncryptedFileActivity.kt
@@ -1,11 +1,31 @@
 package com.imeepwni.jetpack.function.encrypted_file
 
+import android.os.Bundle
+import com.imeepwni.jetpack.R
 import com.imeepwni.jetpack.app.BaseActivity
+import com.imeepwni.jetpack.app.toast
+import kotlinx.android.synthetic.main.activity_encrypted_file.*
 
 /**
  * 作者：Created by guofeng on 2019/10/12
  * 邮箱：feng.guo@bees360.com
  */
 class EncryptedFileActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_encrypted_file)
+
+        initView()
+    }
+
+    private fun initView() {
+        btn_read_file.setOnClickListener {
+            toast("You Click READ Button")
+        }
+        btn_commit_input.setOnClickListener {
+            toast("You Click WRITE Button")
+        }
+    }
 
 }
