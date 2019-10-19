5b60e68eb9a3955db1dcd79d44cb11bbc72baf4e
imeepwni <imeepwni@outlook.com>
2019-10-19 16:03:34 +0800

    add view extension file.

---
+++ app/src/main/java/com/imeepwni/jetpack/app/ViewExtension.kt
@@ -0,0 +1,10 @@
+package com.imeepwni.jetpack.app
+
+import android.widget.TextView
+
+/**
+ * 作者：Created by guofeng on 2019/10/19
+ * 邮箱：feng.guo@bees360.com
+ */
+
+fun TextView.getTrimText() = text.trim().toString()
