7754c72b84bdaceeb4beaada087df69336e5dee7
imeepwni <imeepwni@outlook.com>
2019-11-01 15:31:16 +0800

    show User Demo by using data binding.

--- app/src/main/java/com/imeepwni/jetpack/function/data_binding/DataBindingActivity.kt
+++ app/src/main/java/com/imeepwni/jetpack/function/data_binding/DataBindingActivity.kt
@@ -1,10 +1,23 @@
 package com.imeepwni.jetpack.function.data_binding
 
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.imeepwni.jetpack.R
 import com.imeepwni.jetpack.app.BaseActivity
+import com.imeepwni.jetpack.data.User
+import com.imeepwni.jetpack.databinding.ActivityDataBindingBinding
 
 /**
  * 作者：Created by guofeng on 2019/11/1
  * 邮箱：feng.guo@bees360.com
  */
 class DataBindingActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = DataBindingUtil.setContentView<ActivityDataBindingBinding>(this, R.layout.activity_data_binding)
+
+        binding.user = User(firstName = "JoJo", lastName = "Lee")
+    }
 }
