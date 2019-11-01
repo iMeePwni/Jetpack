ebe2d1380d86e7e34fd42b8d1ab67b305c1fe2e9
imeepwni <imeepwni@outlook.com>
2019-11-01 14:43:26 +0800

    init DataBindingActivity.

---
+++ app/src/main/java/com/imeepwni/jetpack/function/data_binding/DataBindingActivity.kt
@@ -0,0 +1,10 @@
+package com.imeepwni.jetpack.function.data_binding
+
+import com.imeepwni.jetpack.app.BaseActivity
+
+/**
+ * 作者：Created by guofeng on 2019/11/1
+ * 邮箱：feng.guo@bees360.com
+ */
+class DataBindingActivity : BaseActivity() {
+}
