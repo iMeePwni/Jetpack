d0dfa31e86b4e47b4cae91f5242359cd6b7f102a
imeepwni <imeepwni@outlook.com>
2019-11-21 12:16:06 +0800

    #8, add global action to navigate `SearchFragment`.
    1. click `SEARCH` button in `PlantFragment` navigating to `SearchFragment`.

--- app/src/main/java/com/imeepwni/jetpack/function/navigation/plant/PlantFragment.kt
+++ app/src/main/java/com/imeepwni/jetpack/function/navigation/plant/PlantFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.imeepwni.jetpack.R
 import com.imeepwni.jetpack.app.BaseFragment
+import kotlinx.android.synthetic.main.fragment_plant.*
 
 /**
  * 作者：Created by guofeng on 2019/11/19
@@ -15,5 +17,12 @@ class PlantFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_plant, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_search.setOnClickListener {
+            findNavController().navigate(PlantFragmentDirections.actionGlobalSearchFragment())
+        }
     }
 }
