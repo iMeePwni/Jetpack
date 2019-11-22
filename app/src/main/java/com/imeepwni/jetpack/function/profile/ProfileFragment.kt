40156828f494c07adfecbf89269df5b3971c2279
imeepwni <imeepwni@outlook.com>
2019-11-22 11:46:43 +0800

    #8, add `LoginFragment` and `ProfileFragment`.

---
+++ app/src/main/java/com/imeepwni/jetpack/function/profile/ProfileFragment.kt
@@ -0,0 +1,10 @@
+package com.imeepwni.jetpack.function.profile
+
+import com.imeepwni.jetpack.app.BaseFragment
+
+/**
+ * 作者：Created by guofeng on 2019/11/22
+ * 邮箱：feng.guo@bees360.com
+ */
+class ProfileFragment : BaseFragment() {
+}
