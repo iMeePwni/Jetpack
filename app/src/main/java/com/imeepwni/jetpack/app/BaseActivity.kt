97ed7614055d0325420d43e0820ca4190cf28450
imeepwni <imeepwni@outlook.com>
2019-11-27 10:59:48 +0800

    #8, add activity pop animations sample.

--- app/src/main/java/com/imeepwni/jetpack/app/BaseActivity.kt
+++ app/src/main/java/com/imeepwni/jetpack/app/BaseActivity.kt
@@ -2,6 +2,7 @@ package com.imeepwni.jetpack.app
 
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ActivityNavigator
 
 /**
  * 作者：Created by guofeng on 2019/10/12
@@ -9,6 +10,10 @@ import androidx.appcompat.app.AppCompatActivity
  */
 abstract class BaseActivity : AppCompatActivity() {
 
+    override fun finish() {
+        super.finish()
+        ActivityNavigator.applyPopAnimationsToPendingTransition(this)
+    }
 }
 
 internal fun BaseActivity.toast(text: String) {
